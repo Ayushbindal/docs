c4b1f1811b736f59a5bf7360e012e3bb4a3221d9
Ayushbindal <86741007+Ayushbindal@users.noreply.github.com>
2021-09-22 13:30:13 +0530

    Initialize improve/header

---
+++ app/ui-account/client/index.js
@@ -0,0 +1,12 @@
+import './accountFlex.html';
+import './accountIntegrations.html';
+import './accountPreferences.html';
+import './accountProfile.html';
+import './avatar/avatar.html';
+import './avatar/prompt.html';
+import './accountFlex';
+import './accountIntegrations';
+import './accountPreferences';
+import './accountProfile';
+import './avatar/avatar';
+import './avatar/prompt';
