ea5b70e6dc1f3f62fcd7f92a8d44ea2e1caa52cb
Ayushbindal <86741007+Ayushbindal@users.noreply.github.com>
2021-09-22 13:19:25 +0530

    Initialize unsub

---
+++ packages/rocketchat-lib/client/Notifications.js
@@ -0,0 +1,86 @@
+const debug = false;
+RocketChat.Notifications = new class {
+	constructor() {
+		this.logged = Meteor.userId() !== null;
+		this.loginCb = [];
+		Tracker.autorun(() => {
+			if (Meteor.userId() !== null && this.logged === false) {
+				this.loginCb.forEach(cb => cb());
+			}
+			return this.logged = Meteor.userId() !== null;
+		});
+
+		this.streamAll = new Meteor.Streamer('notify-all');
+		this.streamLogged = new Meteor.Streamer('notify-logged');
+		this.streamRoom = new Meteor.Streamer('notify-room');
+		this.streamRoomUsers = new Meteor.Streamer('notify-room-users');
+		this.streamUser = new Meteor.Streamer('notify-user');
+
+		if (debug) {
+			this.onAll(function() {
+				return console.log('RocketChat.Notifications: onAll', arguments);
+			});
+			this.onUser(function() {
+				return console.log('RocketChat.Notifications: onAll', arguments);
+			});
+
+			['notifyRoom', 'notifyUser', 'notifyUsersOfRoom'].forEach(event => {
+				const fn = this[event];
+
+				this[event] = (...args) => {
+					console.log(`RocketChat.Notifications: ${ event }`, args);
+					return fn(...args);
+				};
+			});
+		}
+	}
+
+	onLogin(cb) {
+		this.loginCb.push(cb);
+		if (this.logged) {
+			return cb();
+		}
+	}
+	notifyRoom(room, eventName, ...args) {
+		args.unshift(`${ room }/${ eventName }`);
+		return this.streamRoom.emit.apply(this.streamRoom, args);
+	}
+	notifyUser(userId, eventName, ...args) {
+		args.unshift(`${ userId }/${ eventName }`);
+		return this.streamUser.emit.apply(this.streamUser, args);
+	}
+	notifyUsersOfRoom(room, eventName, ...args) {
+		args.unshift(`${ room }/${ eventName }`);
+		return this.streamRoomUsers.emit.apply(this.streamRoomUsers, args);
+	}
+	onAll(eventName, callback) {
+		return this.streamAll.on(eventName, callback);
+	}
+	onLogged(eventName, callback) {
+		return this.onLogin(() => {
+			return this.streamLogged.on(eventName, callback);
+		});
+	}
+	onRoom(room, eventName, callback) {
+		if (debug) {
+			this.streamRoom.on(room, () => console.log(`RocketChat.Notifications: onRoom ${ room }`, arguments));
+		}
+		return this.streamRoom.on(`${ room }/${ eventName }`, callback);
+	}
+	onUser(eventName, callback) {
+		return this.streamUser.on(`${ Meteor.userId() }/${ eventName }`, callback);
+	}
+	unAll(callback) {
+		return this.streamAll.removeListener('notify', callback);
+	}
+	unLogged(callback) {
+		return this.streamLogged.removeListener('notify', callback);
+	}
+	unRoom(room, eventName, callback) {
+		return this.streamRoom.removeListener(`${ room }/${ eventName }`, callback);
+	}
+	unUser(eventName, callback) {
+		return this.streamUser.removeListener(`${ Meteor.userId() }/${ eventName }`, callback);
+	}
+
+};
