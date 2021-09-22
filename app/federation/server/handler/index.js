eaf4b8880a50c042e4a77abf75035cd5c1b00605
Ayushbindal <86741007+Ayushbindal@users.noreply.github.com>
2021-09-22 14:48:49 +0530

    Initialize change-info-logs-to-debug

---
+++ app/federation/server/handler/index.js
@@ -0,0 +1,81 @@
+import qs from 'querystring';
+
+import { disabled } from '../functions/errors';
+import { clientLogger } from '../lib/logger';
+import { isFederationEnabled } from '../lib/isFederationEnabled';
+import { federationRequestToPeer } from '../lib/http';
+
+export function federationSearchUsers(query) {
+	if (!isFederationEnabled()) {
+		throw disabled('client.searchUsers');
+	}
+
+	// TODO Logger: convert to JSON
+	clientLogger.debug(`searchUsers => query=${ query }`);
+
+	const [username, peerDomain] = query.split('@');
+
+	const uri = `/api/v1/federation.users.search?${ qs.stringify({ username, domain: peerDomain }) }`;
+
+	const { data: { users } } = federationRequestToPeer('GET', peerDomain, uri);
+
+	return users;
+}
+
+export function getUserByUsername(query) {
+	if (!isFederationEnabled()) {
+		throw disabled('client.searchUsers');
+	}
+
+	// TODO Logger: convert to JSON
+	clientLogger.debug(`getUserByUsername => query=${ query }`);
+
+	const [username, peerDomain] = query.split('@');
+
+	const uri = `/api/v1/federation.users.getByUsername?${ qs.stringify({ username }) }`;
+
+	const { data: { user } } = federationRequestToPeer('GET', peerDomain, uri);
+
+	return user;
+}
+
+export function requestEventsFromLatest(domain, fromDomain, contextType, contextQuery, latestEventIds) {
+	if (!isFederationEnabled()) {
+		throw disabled('client.requestEventsFromLatest');
+	}
+
+	// TODO Logger: convert to JSON
+	clientLogger.debug(`requestEventsFromLatest => domain=${ domain } contextType=${ contextType } contextQuery=${ JSON.stringify(contextQuery, null, 2) } latestEventIds=${ latestEventIds.join(', ') }`);
+
+	const uri = '/api/v1/federation.events.requestFromLatest';
+
+	federationRequestToPeer('POST', domain, uri, { fromDomain, contextType, contextQuery, latestEventIds });
+}
+
+
+export function dispatchEvents(domains, events) {
+	if (!isFederationEnabled()) {
+		throw disabled('client.dispatchEvents');
+	}
+
+	domains = [...new Set(domains)];
+
+	// TODO Logger: convert to JSON
+	clientLogger.debug(`dispatchEvents => domains=${ domains.join(', ') } events=${ events.map((e) => JSON.stringify(e, null, 2)) }`);
+
+	const uri = '/api/v1/federation.events.dispatch';
+
+	for (const domain of domains) {
+		federationRequestToPeer('POST', domain, uri, { events }, { ignoreErrors: true });
+	}
+}
+
+export function dispatchEvent(domains, event) {
+	dispatchEvents([...new Set(domains)], [event]);
+}
+
+export function getUpload(domain, fileId) {
+	const { data: { upload, buffer } } = federationRequestToPeer('GET', domain, `/api/v1/federation.uploads?${ qs.stringify({ upload_id: fileId }) }`);
+
+	return { upload, buffer: Buffer.from(buffer) };
+}
