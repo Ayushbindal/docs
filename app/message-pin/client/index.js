5cab6efa06a1e1b96742a7779b6ac0a0885742a1
Ayushbindal <86741007+Ayushbindal@users.noreply.github.com>
2021-09-22 14:24:51 +0530

    Initialize refactor/use-form-types

---
+++ app/message-pin/client/index.js
@@ -0,0 +1,6 @@
+import './actionButton';
+import './pinMessage';
+import './tabBar';
+import './views/pinnedMessages.html';
+import './views/pinnedMessages';
+import './views/stylesheets/messagepin.css';
