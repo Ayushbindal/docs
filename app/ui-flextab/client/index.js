07309c9948d6a5c3066a6b976b713990f0f3562f
Ayushbindal <86741007+Ayushbindal@users.noreply.github.com>
2021-09-22 13:52:00 +0530

    Initialize normalize-forgot-password-endpoint-response

---
+++ app/ui-flextab/client/index.js
@@ -0,0 +1,8 @@
+import './flexTabBar.html';
+import './tabs/inviteUsers.html';
+import './tabs/membersList.html';
+import './tabs/uploadedFilesList.html';
+import './flexTabBar';
+import './tabs/inviteUsers';
+import './tabs/membersList';
+import './tabs/uploadedFilesList';
