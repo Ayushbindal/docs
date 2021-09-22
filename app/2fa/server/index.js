b5900f196afae134fd195cc7e15253118d7836dc
Ayushbindal <86741007+Ayushbindal@users.noreply.github.com>
2021-09-22 13:26:21 +0530

    Initialize 2fa-on-password-reset

---
+++ app/2fa/server/index.js
@@ -0,0 +1,8 @@
+import './startup/settings';
+import './methods/checkCodesRemaining';
+import './methods/disable';
+import './methods/enable';
+import './methods/regenerateCodes';
+import './methods/validateTempToken';
+import './methods/resetPasswordWithTOTP';
+import './loginHandler';
---
+++ app/2fa/server/methods/resetPasswordWithTOTP.js
@@ -0,0 +1,59 @@
+import { Meteor } from 'meteor/meteor';
+import { Users } from '../../../models';
+import { TOTP } from '../lib/totp';
+
+Meteor.methods({
+	'resetPasswordWithTOTP'(userToken, hashedPassword, totpCode) {
+		if (typeof userToken !== 'string' || userToken.trim() === '') {
+			throw new Meteor.Error('totp-invalid-token', 'User Token Missing or Invalid.');
+		}
+
+		const query = {
+			'services.password.reset.token' : userToken,
+		};
+
+		const user = Users.findOne(query, {});
+
+		if (user && user.services && user.services.totp && user.services.totp.enabled === true) {
+			if (!totpCode) {
+				throw new Meteor.Error('totp-required', 'TOTP Required');
+			}
+
+			const verified = TOTP.verify({
+				secret: user.services.totp.secret,
+				token: totpCode,
+				userId: user._id,
+				backupTokens: user.services.totp.hashedBackup,
+			});
+
+			if (verified !== true) {
+				throw new Meteor.Error('totp-invalid', 'TOTP Invalid');
+			}
+
+			try {
+				const result = Meteor.call('resetPassword', userToken, hashedPassword);
+				return result;
+			} catch (e) {
+				// totp-required means the password was reset, but the auto login has failed because of the missing totp code
+				// let's skip this error and call login again, including the totp code this time
+				if (e.error !== 'totp-required') {
+					throw e;
+				}
+			}
+
+			return Meteor.call('login', {
+				totp: {
+					code : totpCode,
+					login: {
+						user: {
+							username: user.username,
+						},
+						password: hashedPassword,
+					},
+				},
+			});
+		} else {
+			return Meteor.call('resetPassword', userToken, hashedPassword);
+		}
+	},
+});
