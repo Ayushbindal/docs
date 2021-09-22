eb2df9289f1497dd5c40f21623d5ab7da6c3ea16
Ayushbindal <86741007+Ayushbindal@users.noreply.github.com>
2021-09-22 13:20:25 +0530

    Initialize apps-engine-only

---
+++ packages/rocketchat-apps/server/converters/index.js
@@ -0,0 +1,13 @@
+import { AppMessagesConverter } from './messages';
+import { AppRoomsConverter } from './rooms';
+import { AppRoomSubscriptionsConverter } from './roomSubscriptions';
+import { AppSettingsConverter } from './settings';
+import { AppUsersConverter } from './users';
+
+export {
+	AppMessagesConverter,
+	AppRoomsConverter,
+	AppSettingsConverter,
+	AppRoomSubscriptionsConverter,
+	AppUsersConverter
+};
