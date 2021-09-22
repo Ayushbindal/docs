eb938ee21b7697d94dac063786dcb0682d778c08
Ayushbindal <86741007+Ayushbindal@users.noreply.github.com>
2021-09-22 13:23:03 +0530

    Initialize rest-get-deleted-messages

---
+++ packages/rocketchat-ui/client/components/icon.js
@@ -0,0 +1,8 @@
+import { FlowRouter } from 'meteor/kadira:flow-router';
+import { Template } from 'meteor/templating';
+
+const baseUrlFix = () => `${ window.location.origin }${ FlowRouter.current().path }`;
+
+Template.icon.helpers({
+	baseUrl: ((isFirefox && isFirefox[1] < 55) || (isChrome && isChrome[1] < 55)) ? baseUrlFix : undefined,
+});
